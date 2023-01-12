ee78a7a1223e2283e7d2c5e8c920457c5a391a90
shake551 <73379887+shake551@users.noreply.github.com>
2023-01-12 07:37:13 +0000

    add model

---
+++ domain/model/article_model.go
@@ -0,0 +1,9 @@
+package model
+
+type Article struct {
+	ID        int64  `json:"id"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	CreatedAt int64  `json:"created_at"`
+	UpdatedAt int64  `json:"updated_at"`
+}
