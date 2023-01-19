9fcae289cbe36ab6903053da731daa0333743cf2
shake551 <73379887+shake551@users.noreply.github.com>
2023-01-19 01:10:31 +0000

    add create article handler

--- interfaces/api/server/handler/article_handler.go
+++ interfaces/api/server/handler/article_handler.go
@@ -7,10 +7,12 @@ import (
 	"strconv"
 
 	"github.com/shake551/go-sql-limit-sample/application/usecase"
+	"github.com/shake551/go-sql-limit-sample/domain/model"
 )
 
 type ArticleHandler interface {
 	GetLimit(w http.ResponseWriter, r *http.Request)
+	Create(w http.ResponseWriter, r *http.Request)
 }
 
 type articleHandler struct {
@@ -59,6 +61,34 @@ func (h *articleHandler) GetLimit(w http.ResponseWriter, r *http.Request) {
 	}
 
 	b, err := json.Marshal(cocktails)
+	if err != nil {
+		log.Printf("failed to parse json. err: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func (h *articleHandler) Create(w http.ResponseWriter, r *http.Request) {
+	body := &model.ArticleParam{}
+	if err := json.NewDecoder(r.Body).Decode(body); err != nil {
+		log.Printf("bad request error. err: %v, body: %v", err, body)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	article, err := h.u.Create(r.Context(), *body)
+	if err != nil {
+		log.Printf("failed to get article. err: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(article)
 	if err != nil {
 		log.Printf("failed to parse json. err: %v", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
