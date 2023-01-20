9f626a6cd7094b5222b6d4d83c42ea75496bb510
shake551 <73379887+shake551@users.noreply.github.com>
2023-01-20 01:40:38 +0000

    update scenario

---
+++ load_test/get_limit.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+const VUS = 1000;
+
+export const options = {
+    stages: [
+        { duration: '30s', target: VUS },
+        { duration: '30s', target: 0 },
+    ]
+}
+
+export default function scenarioFunc() {
+    http.get(`http://localhost/article?limit=20`);
+    sleep(1);
+}
